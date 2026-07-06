b93d3fa369a6b7fdb5835d7a9ba54eda962496b7
Husain Ghulam <husainsaiffee088@gmail.com>
2026-07-07 01:41:02 +0530

    chore: bump version to 1.0.7 and update author to Husain Ghulam

--- trace_memory/__init__.py
+++ trace_memory/__init__.py
@@ -25,8 +25,8 @@ from .ctree import CTree, Node, TopicNode, MessageNode
 from .vector_db import VectorDatabase, ConversationVector
 from .prompt_synthesizer import PromptSynthesizer
 
-__version__ = "1.0.6"
-__author__  = "Husain"
+__version__ = "1.0.7"
+__author__  = "Husain Ghulam"
 __license__ = "Apache-2.0"
 
 __all__ = [
